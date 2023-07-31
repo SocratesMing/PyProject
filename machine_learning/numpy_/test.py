22241d4d486bb57dc0322ec1f7d2a760fd9f514b
sututu <sututu666@hotmail.com>
2023-07-31 21:28:57 +0800

    sklearn - quickstart

---
+++ machine_learning/numpy_/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+X = 1.0 / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
+print(np.arange(1, 11))
+print(np.arange(0, 10))
+print(np.arange(0, 10)[:, np.newaxis])
+print(X)
